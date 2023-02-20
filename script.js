21c9ccb414e72e4d730967c7fe7e29b1dd269faf
Alejo <alejo25.gab2005@gmail.com>
2023-02-20 18:06:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+/*const api = 'https://randomuser.me/api/?gender=male';
+
+fetch(api)
+.then(response => response.json())
+.then(json => console.log(json))
+.catch(err => console.log('Solicitud fallida', err));*/
+
+document.getElementById("imprimir").addEventListener('click', function() {
+    window.print();
+});
+
+document.getElementById("imprimir").addEventListener('mouseover', function () {
+    document.body.style.cursor = 'pointer';
+});
+
+document.getElementById("imprimir").addEventListener('mouseout', function () {
+    document.body.style.cursor = 'default';
+});
